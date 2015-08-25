d50e07f9455ffee22d387915e5016a12613ec078
Alex <alexsantorineos@gmail.com>
2015-08-25 16:46:02 -0500

    Initial Commit

---
+++ CansBottlesAndBags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CansBottlesAndBags
+//
+//  Created by Alex Santorineos on 8/25/15.
+//  Copyright (c) 2015 Alex Santorineos & Chris Scalucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
